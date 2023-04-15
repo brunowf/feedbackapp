d360a1dcd7bfc1aca6ed64c6334d94bed57e4970
Bruno Weiler Foletto <brunoweiller@hotmail.com>
2023-04-15 18:48:12 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+    console.log(newFeedback);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <Router>
+      <Header></Header>
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback}></FeedbackForm>
+                <FeedbackStats feedback={feedback}></FeedbackStats>
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                ></FeedbackList>
+              </>
+            }
+          ></Route>
+        </Routes>
+        <Routes>
+          <Route path="/about" element={<AboutPage></AboutPage>}></Route>
+        </Routes>
+        <AboutIconLink></AboutIconLink>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
